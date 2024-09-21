ad75767e7518c99ac6d650f06be0c63868103531
theoriginalcancercell <50221835+theoriginalcancercell@users.noreply.github.com>
2024-09-21 00:16:33 -0600

    Tileset and spikes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float maxHorizontalSpeed;
     [SerializeField] float jumpForce;
     [SerializeField] Collider2D groundedCollider;
+    [SerializeField] Collider2D circleCollider;
 
     private void Update()
     {
@@ -23,17 +24,33 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if (Mathf.Sign(rb.velocity.x) != Mathf.Sign(Input.GetAxis("Horizontal")) || Mathf.Abs(rb.velocity.x) < maxHorizontalSpeed ) { 
+        if ( !circleCollider.IsTouchingLayers() &&
+            (Mathf.Sign(rb.velocity.x) != Mathf.Sign(Input.GetAxis("Horizontal")) || Mathf.Abs(rb.velocity.x) < maxHorizontalSpeed)) { 
             rb.AddForce(new Vector2(horizontalAcceleration * Input.GetAxis("Horizontal"), 0));
         }
-        else
+        //else
+        //{
+        //    Vector2 targetVelocity = new Vector2(Mathf.Sign(Input.GetAxis("Horizontal")) * maxHorizontalSpeed, rb.velocity.y);
+        //    Vector2 deltaVelocity = targetVelocity - rb.velocity;
+        //    rb.AddForce(deltaVelocity);
+        //}
+
+        if (rb.velocity.y > -50)
         {
-            rb.velocity = new Vector2(Mathf.Sign(Input.GetAxis("Horizontal")) * maxHorizontalSpeed, rb.velocity.y);
+            rb.AddForce(new Vector2(0, -50 - rb.velocity.y));
         }
     }
 
     public void AddImpulse(Vector2 impulse)
     {
         rb.AddForce(impulse, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Damaging")
+        {
+            print("You are dead");
+        }
     }
 }
