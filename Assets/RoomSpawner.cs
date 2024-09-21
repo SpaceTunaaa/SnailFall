81e7051d865281439ba9fea7d6b7f1bee03ef071
theoriginalcancercell <50221835+theoriginalcancercell@users.noreply.github.com>
2024-09-20 22:24:08 -0600

    Level Design Basics (Camera movement coming soon)

---
+++ Assets/RoomSpawner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSpawner : MonoBehaviour
+{
+    [SerializeField] List<GameObject> rooms = new List<GameObject>();
+    [SerializeField] int numberOfRooms;
+    [SerializeField] float roomHeight;
+
+    private void Start()
+    {
+        for (int i = 1; i+1 <= numberOfRooms + 1; i++)
+        {
+            Instantiate(rooms[Random.Range(0, rooms.Count)], new Vector2(0, roomHeight * -i), Quaternion.identity);
+        }
+    }
+}
